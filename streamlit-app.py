a25bcdb17c02023a429e533405f3fd9b0e9228ea
Adam Staber <staber@gmail.com>
2024-05-30 09:03:16 -0400

    image size

--- streamlit-app.py
+++ streamlit-app.py
@@ -14,6 +14,6 @@ rows = execute_query(conn.table("players").select("*").order("shots", desc=True)
 # Print results.
 for row in rows.data:
     # st.image({row['avatar']}) # images can't be gifs
-    st.image("https://storage.googleapis.com/ts_assets_prod-roster_full_photos/106848656/original/b2d721cd-de4b-4a57-8536-f9aa2ed8cd18.jpg")
+    st.image("https://storage.googleapis.com/ts_assets_prod-roster_full_photos/106848656/original/b2d721cd-de4b-4a57-8536-f9aa2ed8cd18.jpg", width=64)
     # st.markdown(f"[![Click me]({row['avatar']})](https://streamlit.io)")
     st.write(f"{row['first']} {row['last']} : {row['shots']}")
