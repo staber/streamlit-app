969fe95926fbfe7547ee84b7d69be2cc5349aff4
Adam Staber <staber@gmail.com>
2024-06-03 11:22:35 -0400

    Insert row to player db and update total table

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,8 +1,22 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection, execute_query
 
+# Add row to player table
 def add_shots(id, name, shots, date):
-   st.toast(str(id) + ": Add " + str(shots) + " for " + name + " on " + str(date))
+   st.toast(str(id) + ": Add " + str(shots) + " shots for " + str(name) + " on " + date.strftime("%Y-%m-%d"))
+   execute_query(conn.table(name).insert(
+       [{"submitted_on": date.strftime("%Y-%m-%d"), "shots": shots}], count="None"
+   ), ttl=0,)
+   update_total_shots(id, name)
+
+# Update the team table to show the newly updated player total
+def update_total_shots(id, player):
+    shots = 0
+    player_table = execute_query(conn.table(player).select("*"), ttl=0)
+    for row in player_table.data:
+        shots = shots + row['shots']
+    st.toast(player + " has " + str(shots) + " shots total.")
+    execute_query(conn.table("players").update({"shots": shots}, count="None").eq("id", id), ttl=0)
 
 # Initialize connection.
 conn = st.connection(
@@ -22,15 +36,17 @@ button[title="View fullscreen"]{
 st.markdown(hide_img_fs, unsafe_allow_html=True)
 
 # Perform query.
-rows = execute_query(conn.table("players").select("*").order("shots", desc=True), ttl=0)
+shooting_leaderboard = execute_query(conn.table("players").select("*").order("shots", desc=True), ttl=0)
 
+# Setup Main Page
 tab1, tab2 = st.tabs(["🏒 Shots", "🏃 Running"])
 
+# Shooting Leaderboard Tab
 with tab1:
     st.header("Shooting Leaderboard")
 
     # Print results.
-    for row in rows.data:
+    for row in shooting_leaderboard.data:
         with st.expander(f"{row['first']} {row['last']} : {row['shots']}", expanded=False):
             # col1, col2, col3 = st.columns([0.2, 0.5, 0.3])
             # col1.image("https://storage.googleapis.com/ts_assets_prod-roster_full_photos/106848656/original/b2d721cd-de4b-4a57-8536-f9aa2ed8cd18.jpg", width=64, clamp=True)
@@ -46,12 +62,13 @@ with tab1:
                         type="primary",
                         use_container_width=True):
                     if shots > 0:
-                        add_shots({row['id']},{row['last']},shots,date)
+                        add_shots(row['id'], f"{row['first']}_{row['last']}", shots, date)
                     else:
                         st.toast("Enter a number greater than 0")
         
         # st.image({row['avatar']}) # images can't be gifs
         # st.markdown(f"[![Click me]({row['avatar']})](https://streamlit.io)")
 
+# Running Leaderboard Tab
 with tab2:
     st.header("Mile Time Leaderboard")
